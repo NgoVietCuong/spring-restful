1da5675aaceafaaf461aff84a80ed0bee25acffd
ngo cuong <ngocuong972000@gmail.com>
2025-01-16 18:19:17 +0700

    user restful api

--- src/main/java/com/nvc/spring_boot/controller/UserController.java
+++ src/main/java/com/nvc/spring_boot/controller/UserController.java
@@ -1,19 +1,51 @@
 package com.nvc.spring_boot.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nvc.spring_boot.entity.User;
+import com.nvc.spring_boot.service.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 public class UserController {
+    private final UserService userService;
 
-    @GetMapping("/user/create")
-    public String create() {
-        User user = new User();
-        user.setEmail("ngocuong@gmail.com");
-        user.setName("Cuong");
-        user.setPassword("123456");
-        return "create user";
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/list")
+    public List<User> getList() {
+        return userService.getList();
+    }
+
+    @GetMapping("/user/{id}")
+    public User findUser(@PathVariable("id") Long id) {
+        return userService.findUser(id);
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+    
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        User newUser = userService.createUser(user);
+        return newUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "Delete User";
     }
 }
--- src/main/java/com/nvc/spring_boot/service/UserService.java
+++ src/main/java/com/nvc/spring_boot/service/UserService.java
@@ -1,5 +1,47 @@
 package com.nvc.spring_boot.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.nvc.spring_boot.entity.User;
+import com.nvc.spring_boot.repository.UserRepository;
+
+@Service
 public class UserService {
-    
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getList() {
+        return userRepository.findAll();
+    }
+
+    public User findUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User updateUser(User userData) {
+        User user = findUser(userData.getId());
+        if (user != null) {
+            user.setName(userData.getName());
+            user.setEmail(userData.getEmail());
+            user.setPassword(userData.getPassword());
+            return userRepository.save(user);
+        }
+
+        return user;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
 }
