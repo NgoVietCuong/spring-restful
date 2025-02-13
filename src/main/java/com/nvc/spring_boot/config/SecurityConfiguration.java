be2c4d952c3f91832362540dc87bcc41cf9b6a3a
ngo cuong <ngocuong972000@gmail.com>
2025-02-13 17:43:18 +0700

    fix cors

--- src/main/java/com/nvc/spring_boot/config/SecurityConfiguration.java
+++ src/main/java/com/nvc/spring_boot/config/SecurityConfiguration.java
@@ -22,6 +22,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         http
                 .csrf(c -> c.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authz -> authz.requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 )
