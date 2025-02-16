f4ec9a67dd620e619981364eb7cf9d7c5ba78836
ngo cuong <ngocuong972000@gmail.com>
2025-02-16 22:47:52 +0700

    update query company with filter and pagination

--- src/main/java/com/nvc/spring_boot/repository/CompanyRepository.java
+++ src/main/java/com/nvc/spring_boot/repository/CompanyRepository.java
@@ -1,9 +1,11 @@
 package com.nvc.spring_boot.repository;
 
 import com.nvc.spring_boot.domain.Company;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
+public interface CompanyRepository extends JpaRepository<Company, Long>, JpaSpecificationExecutor<Company> {
 }
